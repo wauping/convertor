58ed1436ced9d130f782f8996d4d21e578e5dd62
wauping <waup98@gmail.com>
2023-12-22 13:29:33 +0500

    123

---
+++ convertor/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace convertor
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ convertor/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace convertor
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
